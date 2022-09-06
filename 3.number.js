35bb586e2460cd95be3be854f98ad5817a6ec61f
jkim8 <juno0218@naver.com>
2022-09-06 17:33:35 -0400

    Number Type

---
+++ 3.number.js
@@ -0,0 +1,16 @@
+let integer = 123; //정수
+let nagative = -123; //음수
+let double = 1.243; // 실수
+
+let binery = 0b1111011; //2진수
+let octal = 0o173; //8진수
+let hex = 0x7b; //16진수
+
+console.log(binery);
+console.log(octal);
+console.log(hex);
+
+console.log(0 / 123); //0
+console.log(123 / 0); //Infinity
+console.log(123 / -0); //-Infinity
+console.log(123 / "text"); // NaN (Not a Number)
