bc248a239336ba4adacef589988c2afd826fc9ac
jkim8 <juno0218@naver.com>
2022-09-07 11:38:46 -0400

    boolean & null,undefined

---
+++ 5.boolean.js
@@ -0,0 +1,24 @@
+let 참 = true;
+let 거짓 = false;
+
+//활용 예
+
+let isFree = true;
+let isActivate = false;
+let isEnrolled = true;
+
+//falsy 거짓은 값
+console.log(!!0);
+console.log(!!-0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+
+//fruty 참인 값
+console.log(!!1);
+console.log(!!-1);
+console.log(!!'text');
+console.log(!!{});
+console.log(!![]);
+console.log(!!Infinity);
---
+++ 6.empty.js
@@ -0,0 +1,13 @@
+//  null, undefined
+
+let variable;
+console.log(variable);
+
+variable = null;
+console.log(variable);
+
+//undefined는 값이 정해지 않았고
+//null은 값이 없다가 정해진 것임
+
+console.log(typeof null); //object가 됨
+console.log(typeof undefined);
