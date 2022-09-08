7deba0bfb47d5404f916b4e85efa143bc7edba08
jkim8 <juno0218@naver.com>
2022-09-08 09:24:02 -0400

    condition and tenary

---
+++ 1.if.js
@@ -0,0 +1,13 @@
+//조건문 Conditional Statement
+// if(조건) {}
+// if(조건) {} else {}
+// if(조건) {} else if (조건)  {} else {}
+
+let fruit = 'apple';
+if (fruit === 'apple') {
+  console.log('🍎');
+}
+
+if (2 > 1) {
+  console.log('출력되면 안됨');
+}
---
+++ 2.tenary.js
@@ -0,0 +1,10 @@
+//삼항 조건 연산자 Tenary Operator
+//조건식 ? 참인경우 : 거짓인 경우
+
+let fruit = 'apple';
+
+fruit === 'apple' ? console.log('🍎') : console.log('❌');
+
+let emoji = fruit === 'apple' ? '🍎' : '❌';
+
+console.log(emoji);
