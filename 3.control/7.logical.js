0c911223f02fb98be58b9d635643f61c04525c76
jkim8 <juno0218@naver.com>
2022-09-08 15:14:03 -0400

    logical

---
+++ 3.control/7.logical.js
@@ -0,0 +1,10 @@
+//논리 연산자 Logical operator
+// && 그리고
+//  || 또는
+// ! 부정
+// !! 불리언 값으로 변환
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
