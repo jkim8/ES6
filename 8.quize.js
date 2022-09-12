358a90e27f6a160b3631a1e92b182153c0fb4851
jkim8 <juno0218@naver.com>
2022-09-12 15:28:05 -0400

    quize

---
+++ 8.quize.js
@@ -0,0 +1,15 @@
+const text = 'Hello World!';
+
+for (let i = 0; i < text.length; i++) {
+  console.log(text[i]);
+}
+
+const ids = 'user1. user2, user3, user4';
+
+const array = ids.split(', ');
+console.log(array);
+
+setInterval(() => {
+  const now = new Date();
+  console.log(now.toLocaleDateString('ko-KR'));
+}, 1000);
