c8844555d95320bd56e00159f473d13b51838bd2
jkim8 <juno0218@naver.com>
2022-09-12 14:28:57 -0400

    math

---
+++ 5.math.js
@@ -0,0 +1,38 @@
+//Math
+
+console.log(Math.E); //오일러의 상수, 자연로그의 밑
+console.log(Math.PI);
+
+//static method
+//절대값
+console.log(Math.abs(-10));
+
+//소수점 이하를 올림
+console.log(Math.ceil(1.3));
+
+//소수점 이하는 애림
+console.log(Math.floor(1.7));
+
+//소수점 이하를 반올림
+console.log(Math.round(1.7));
+console.log(Math.round(1.2));
+
+//정수만 반환
+console.log(Math.trunc(1.9));
+
+//최대 최소값을 찾기
+console.log(Math.max(1, 2, 3, 4));
+console.log(Math.min(1, 2, 3, 4));
+
+//거듭제곱
+console.log(3 ** 2);
+console.log(Math.pow(3, 2));
+
+//제곱근
+console.log(Math.sqrt(144));
+
+//랜덤한 값을 반환
+console.log(Math.random());
+
+//1~10 램덤 숫자 찾으려면
+console.log(Math.floor(Math.random() * 10 + 1));
