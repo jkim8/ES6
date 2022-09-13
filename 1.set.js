bc5502199c2587ede71a437dbe8208a0c37409a5
jkim8 <juno0218@naver.com>
2022-09-13 17:58:01 -0400

    set

---
+++ 1.set.js
@@ -0,0 +1,49 @@
+// Set
+const set = new Set([1, 2, 3]);
+console.log(set);
+
+//set 사이즈 확인
+console.log(set.size);
+
+//존재 하는 지 확인
+console.log(set.has(2)); //true
+console.log(set.has(4)); //false
+
+//순회
+set.forEach((item) => console.log(item));
+for (const value of set.values()) {
+  console.log(value);
+}
+
+// 추가
+set.add(6);
+console.log(set);
+set.add(6); //중복된 값 추가 안됨
+console.log(set);
+
+//삭제
+set.delete(6);
+console.log(set);
+
+//전부 삭제
+set.clear();
+console.log(set);
+
+//오브젝트 세트
+const obj1 = { name: 'apple', price: 8 };
+const obj2 = { name: 'banana', price: 10 };
+const objs = new Set([obj1, obj2]);
+console.log(objs);
+
+//퀴즈
+obj1.price = 10;
+objs.add(obj1);
+console.log(objs);
+
+//퀴즈
+const obj3 = { name: 'banana', price: 10 };
+objs.add(obj3);
+console.log(objs);
+
+obj3.price = 8;
+console.log(objs);
